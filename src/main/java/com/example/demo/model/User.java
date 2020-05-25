c1e414b9dadc09cbee08335d0b75f9c48cb20cd5
孤舟 <@>
2020-05-25 20:20:50 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int id;
+    public  String name;
+    public  String password;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password=password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
