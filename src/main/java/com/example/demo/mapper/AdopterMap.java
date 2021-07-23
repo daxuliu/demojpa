a11db9394943e476f460cce97bd764849eef9a8b
xuliu <xuliu@aibee.com>
2021-07-23 16:04:05 +0800

    hibernate query

--- src/main/java/com/example/demo/mapper/AdopterMap.java
+++ src/main/java/com/example/demo/mapper/AdopterMap.java
@@ -3,6 +3,9 @@ package com.example.demo.mapper;
 import com.example.demo.model.Adopter;
 import com.example.demo.model.Adoption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AdopterMap extends JpaRepository<Adopter, Integer> {
+    @Query("select  a from Adopter a where a.de like ?1")
+    public Adopter findAdopterByDeLike(String de);
 }
